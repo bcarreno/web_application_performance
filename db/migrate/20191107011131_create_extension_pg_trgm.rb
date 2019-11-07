36932def7553f5fafcf9be169d67cd4ef23f7480
Braulio Carreno <bcarreno@yahoo.com>
2019-11-06 21:19:59 -0500

    Use rails helpers

--- db/migrate/20191107011131_create_extension_pg_trgm.rb
+++ db/migrate/20191107011131_create_extension_pg_trgm.rb
@@ -1,9 +1,5 @@
 class CreateExtensionPgTrgm < ActiveRecord::Migration[5.2]
-  def up
-    execute 'create extension if not exists pg_trgm'
-  end
-
-  def down
-    execute 'drop extension pg_trgm'
+  def change
+    enable_extension 'pg_trgm'
   end
 end
--- db/migrate/20191107011741_add_index_to_players_on_primary_name.rb
+++ db/migrate/20191107011741_add_index_to_players_on_primary_name.rb
@@ -1,9 +1,5 @@
 class AddIndexToPlayersOnPrimaryName < ActiveRecord::Migration[5.2]
-  def up
-    execute 'create index index_players_on_primary_name on players using gin (primary_name gin_trgm_ops)'
-  end
-
-  def down
-    execute 'drop index index_players_on_primary_name'
+  def change
+    add_index :players, :primary_name, using: 'gin', opclass: :gin_trgm_ops
   end
 end
